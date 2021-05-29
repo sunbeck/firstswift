4a52f0fae18fec16d59ac9d799b6cee1b3812c7f
sunbeck <344079651@qq.com>
2021-05-29 22:53:38 +0800

    初始化

--- swift_first/swift_first/ViewController.swift
+++ swift_first/swift_first/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hellow swift")
+        print("hellow swift 2222")
         // Do any additional setup after loading the view.
     }
 
