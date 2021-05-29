602936d279bc8393055aa9a33f5be5111a9f89ee
sunbeck <344079651@qq.com>
2021-05-29 22:35:29 +0800

    初始化项目

---
+++ firstSwift/firstSwift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  firstSwift
+//
+//  Created by Beck on 2021/5/29.
+//  Copyright © 2021 Beck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hellow swift")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
